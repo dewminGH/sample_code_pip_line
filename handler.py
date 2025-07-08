88cb7480b84f07c7dd5aa5ace663ce5fa7684c1d
dewminGH <trashancsgo@gmail.com>
2025-07-08 23:24:31 +0530

    test-pip-line

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v4.0! Your function executed successfully!"
+        "message": "pip2XXX Go Serverless v4.0! Your function executed successfully!"
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
